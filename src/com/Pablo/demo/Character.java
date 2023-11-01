8eb9e8851b268b5c173e508f72efb30baf1c49af
PabloDiazTumba <pablodiaz147@gmail.com>
2023-11-01 18:58:01 +0100

    First commit

--- src/com/Pablo/demo/Character.java
+++ src/com/Pablo/demo/Character.java
@@ -1,4 +1,21 @@
 package com.Pablo.demo;
 
-public class Character {
+public abstract class Character {
+
+    // Variables / Attributes all characters have
+    public String name;
+    public int maxHP, hp, xp;
+
+    // Constructor
+    public Character(String name, int maxHP, int xp) {
+        this.name = name;
+        this.maxHP = maxHP;
+        this.xp = xp;
+        this.hp = maxHP;
+
+    }
+
+    // methods every character has
+    public abstract int attack();
+    public abstract int defend();
 }
