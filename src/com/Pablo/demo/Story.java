0e7b7776dd0c10c0d98184f7e521310198644e3d
PabloDiazTumba <pablodiaz147@gmail.com>
2023-11-08 16:52:41 +0100

    First commit

--- src/com/Pablo/demo/Story.java
+++ src/com/Pablo/demo/Story.java
@@ -1,5 +1,110 @@
 package com.Pablo.demo;
 
+// Class that does nothing but stores methods to print out every part of the story
 public class Story {
-    
+
+    public static void printIntro() {
+        GameLogic.clearConsole();
+        GameLogic.printSeperator(30);
+        System.out.println("STORY");
+        GameLogic.printSeperator(30);
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        GameLogic.anythingToContinue();
+    }
+
+    public static void printFirstActIntro() {
+        GameLogic.clearConsole();
+        GameLogic.printSeperator(30);
+        System.out.println("STORY");
+        GameLogic.printSeperator(30);
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        GameLogic.anythingToContinue();
+    }
+
+    public static void printFirstActOutro() {
+        GameLogic.clearConsole();
+        GameLogic.printSeperator(30);
+        System.out.println("STORY");
+        GameLogic.printSeperator(30);
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        GameLogic.anythingToContinue();
+    }
+
+    public static void printSecondActIntro() {
+        GameLogic.clearConsole();
+        GameLogic.printSeperator(30);
+        System.out.println("STORY");
+        GameLogic.printSeperator(30);
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        GameLogic.anythingToContinue();
+    }
+
+    public static void printSecondActOutro() {
+        GameLogic.clearConsole();
+        GameLogic.printSeperator(30);
+        System.out.println("STORY");
+        GameLogic.printSeperator(30);
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        GameLogic.anythingToContinue();
+    }
+
+    public static void printThirdActIntro() {
+        GameLogic.clearConsole();
+        GameLogic.printSeperator(30);
+        System.out.println("STORY");
+        GameLogic.printSeperator(30);
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        GameLogic.anythingToContinue();
+    }
+
+    public static void printThirdActOutro() {
+        GameLogic.clearConsole();
+        GameLogic.printSeperator(30);
+        System.out.println("STORY");
+        GameLogic.printSeperator(30);
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        System.out.println("STORY LINE HERE");
+        GameLogic.anythingToContinue();
+    }
+        public static void printFourthActIntro() {
+            GameLogic.clearConsole();
+            GameLogic.printSeperator(30);
+            System.out.println("STORY");
+            GameLogic.printSeperator(30);
+            System.out.println("STORY LINE HERE");
+            System.out.println("STORY LINE HERE");
+            System.out.println("STORY LINE HERE");
+            System.out.println("STORY LINE HERE");
+            GameLogic.anythingToContinue();
+    }
+
+    public static void printEnd(Player player) {
+        GameLogic.clearConsole();
+        GameLogic.printSeperator(30);
+        System.out.println("STORY");
+        GameLogic.printSeperator(30);
+        System.out.println("Congratulations, " + player.name + "! You defeated the Evil Emperor and saved the world!");
+        GameLogic.printSeperator(30);
+        System.out.println("This game was developed by Pablo Diaz from STI Liljeholmen");
+        System.out.println("I hope you enjoyed it");
+    }
 }
