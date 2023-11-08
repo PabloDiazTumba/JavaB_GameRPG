11dfefa872f06c9f1ca4f642488fe99b14479304
PabloDiazTumba <pablodiaz147@gmail.com>
2023-11-08 18:01:16 +0100

    First commit

--- src/com/Pablo/demo/GameLogic.java
+++ src/com/Pablo/demo/GameLogic.java
@@ -8,8 +8,14 @@ public class GameLogic {
 
     public static boolean isRunning;
 
+    // Random encounters
+    public static String[] encounters = {"Battle", "Battle", "Battle", "Rest", "Rest"};
+
+    // Enemy names
+    public static String[] enemies = {"Ogre", "Ogre", "Goblin", "Goblin", "Stone Elemental"};
+
     // Story elements
-    public static int place = 0, act;
+    public static int place = 0, act = 1;
     public static String[] places = {"Everlasting Mountains", "Haunted Landlines",
             "Castle of the Evil Emperor", "Throne room"};
 
@@ -93,9 +99,77 @@ public class GameLogic {
         gameLoop();
     }
 
+    // Method that changes games values based on player xp
+    public static void checkAct() {
+        if (player.xp >= 10 && act == 1) {
+            // Increament act and place
+            act = 2;
+            place = 1;
+            // Story
+            Story.printFirstActOutro();
+            // Let player level up
+            player.chooseTrait();
+            // Story
+            Story.printSecondActIntro();
+            // Assign new values to enemies
+            enemies[0] = "Evil Mercenary";
+            enemies[1] = "Evil Mercenary";
+            enemies[2] = "Henchman of the Evil Emperor";
+            enemies[3] = "Henchman of the Evil Emperor";
+            enemies[4] = "Henchman of the Evil Emperor";
+            // Assign new values to encounters
+            encounters[0] = "Battle";
+            encounters[1] = "Battle";
+            encounters[2] = "Battle";
+            encounters[3] = "Battle";
+            encounters[4] = "Shop";
+            // Fully heal the player
+            player.hp = player.maxHP;
+        } else if (player.xp >= 50 && act == 2) {
+            act = 3;
+            place = 2;
+            // Story
+            Story.printSecondActOutro();
+            // Level up
+            player.chooseTrait();
+            // Story
+            Story.printThirdActIntro();
+        } else if (player.xp >= 100 && act == 3) {
+            act = 4;
+            place = 3;
+            // Story
+            Story.printThirdActOutro();
+            // Level up
+            player.chooseTrait();
+            // Story
+            Story.printFourthActIntro();
+            // Fully heal the player
+            player.hp = player.maxHP;
+            // Final battle
+
+        }
+    }
+
+    // Method to calculate a random encounter
+    public static void randomEncounter() {
+        // Random number between 0 and the length of the encounter array
+        int encounter = (int) (Math.random() * encounters.length);
+        // Calling the respective methods
+        if (encounters[encounter].equals("Battle")) {
+            // Random battle();
+        }else if (encounters[encounter].equals("Rest")) {
+            // Take rest();
+        }else {
+            // Shop();
+        }
+    }
+
     // Method to continue the journey
     public static void continueJourney() {
-
+        // check if act must be increased
+        checkAct();
+        if (act != 4)
+            randomEncounter();
     }
 
     // Printing out important info about player
