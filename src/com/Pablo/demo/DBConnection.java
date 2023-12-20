00aa6b615e647925a260592a01a2639d7e87f5d4
PabloDiazTumba <pablodiaz147@gmail.com>
2023-12-20 09:02:36 +0100

    First commit

--- src/com/Pablo/demo/DBConnection.java
+++ src/com/Pablo/demo/DBConnection.java
@@ -2,5 +2,10 @@ package com.Pablo.demo;
 
 public class DBConnection {
 
-    
+    private String URL = "jdbc:mariadb://localhost:3306/DungeonRun";
+
+    private String USER = "root";
+
+    private String password = "Admin";
+
 }
